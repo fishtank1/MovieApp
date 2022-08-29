c3b66f2b52f81838331da3b29cbd9284e6226168
fishtank <senguptasaumyajeet@outlook.com>
2022-08-30 01:51:37 +0530

    finally we have a fully functional search bar grabbing data from API and showing results

--- script.js
+++ script.js
@@ -2,47 +2,76 @@
 // Details API Link: http://www.omdbapi.com/?i=tt2705436&apikey=94397865
 
 const movieSearchBox = document.getElementById('movie-search-box');
-const searchList = document.getElementById('search-list-item-res');
+const searchList = document.getElementById('search-list');
+const searchBtn = document.querySelector('.search-btn');
+const resultGrid = document.getElementById('result-grid');
 
 // load movies from API
 async function loadMovies(searchTerm){
-    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=94397865`;
     const res = await fetch(`${URL}`);
     const data = await res.json();
-    // console.log(data.Search);
     if(data.Response == "True") displayMovieList(data.Search);
+}
+
+function searchBoxBoderTrigger() {
+    if(movieSearchBox.value.length > 0) {
+        movieSearchBox.style.borderBottomLeftRadius = "0px";
+        searchBtn.style.borderBottomRightRadius = "0px";    
+    } else {
+        movieSearchBox.style.borderBottomLeftRadius = "25px";
+        searchBtn.style.borderBottomRightRadius = "25px";
+    }
 }
 
 function findMovies(){
     let searchTerm = (movieSearchBox.value).trim();
     if(searchTerm.length > 0){
-        searchList.style.display = "block";
+        searchList.classList.remove('hide-search-list');
         loadMovies(searchTerm);
     } else {
-        searchList.style.display = "none";
+        searchList.classList.add('hide-search-list');
     }
 }
 
 function displayMovieList(movies){
+    searchBoxBoderTrigger();
     searchList.innerHTML = "";
     for(let idx = 0; idx < movies.length; idx++){
         let movieListItem = document.createElement('div');
         movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
         movieListItem.classList.add('search-list-item');
-        if(movies[idx].Poster != "N/A")
-            moviePoster = movies[idx].Poster;
-        else 
-            moviePoster = "image_not_found.png";
+        
+        // Hits another API where Actors working in the film are stored
+        fetch(`http://www.omdbapi.com/?i=${movies[idx].imdbID}&apikey=94397865`)
+        .then((response) => response.json())
+        .then((data) => {
+            if(movies[idx].Poster != "N/A")
+                moviePoster = movies[idx].Poster;
+            else 
+                    moviePoster = "notfound.png";
+                
+            movieListItem.innerHTML = `
+                <div class = "search-item-thumbnail shadow-md">
+                    <img class="rounded" src = "${moviePoster}">
+                </div>
+                <div class = "search-item-info">
+                    <h3>${movies[idx].Title}</h3>
+                    <p>${movies[idx].Year}</p>
+                    <p>${data.Actors}</p>
+                </div>
+                `;
+            });
 
-        movieListItem.innerHTML = `
-        <div class = "search-item-thumbnail">
-            <img src = "${moviePoster}">
-        </div>
-        <div class = "search-item-info">
-            <h3>${movies[idx].Title}</h3>
-            <p>${movies[idx].Year}</p>
-        </div>
-        `;
+        // movieListItem.innerHTML = `
+        // <div class = "search-item-thumbnail shadow-md">
+        //     <img class="rounded" src = "${moviePoster}">
+        // </div>
+        // <div class = "search-item-info">
+        //     <h3>${movies[idx].Title}</h3>
+        //     <p>${movies[idx].Year}</p>
+        // </div>
+        // `;
         searchList.appendChild(movieListItem);
     }
     loadMovieDetails();
--- searchBarInteraction.js
+++ searchBarInteraction.js
@@ -2,25 +2,13 @@ let searchBar = document.querySelector('.search-query');
 let searchBtn = document.querySelector('.search-btn');
 let searchResult = document.getElementById('search-list');
 
-// Listner to handle search icon button press/click
-searchBtn.addEventListener('click', () => {
-    if(searchBar.value != '') {
-        searchBar.style.borderBottomLeftRadius = "0px";
-        searchBtn.style.borderBottomRightRadius = "0px";
-        searchResult.style.display = "block";
-    } {
-        searchBar.placeholder = "Search for your movie"
-    }
-});
+function myFunction() {
+    searchResult.style.display = "none";
+    searchBar.placeholder = "Search for your movie"
+    searchBar.style.borderBottomLeftRadius = "25px";
+    searchBtn.style.borderBottomRightRadius = "25px";
+}
 
 // Listner to handle enter key press
-searchBar.addEventListener('keyup',(event) => {
-    if(event.keyCode == 13 && searchBar.value.length > 0) {
-        searchResult.style.display = "block";
-        searchBar.style.borderBottomLeftRadius = "0px";
-        searchBtn.style.borderBottomRightRadius = "0px";
-    } else {
-        searchBar.placeholder = "Search for your movie"
-    }
-    
-});
+searchBar.addEventListener('keyup', myFunction);
+document.addEventListener('keyup', myFunction);
